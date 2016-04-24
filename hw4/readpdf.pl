44175184b78d908d9cad98b868c96ccc1ce8f9e0
Jinyi <jguo32@jhu.edu>
2016-04-24 15:07:23 -0400

    .

--- hw4/readpdf.pl
+++ hw4/readpdf.pl
@@ -1,11 +1,24 @@
-# use PDF::API2;
+use PDF::API2;
+use FileHandle;
+use strict;
 use CAM::PDF;
-# use CAM::PDF::PageText;
+use CAM::PDF::PageText;
 
 &main;
 
 sub main {
-	$filename = "420distributions2.pdf";
+	my $filename = "cv - eisner.pdf";
+	# open my $fh, $filename or die $@;
+	# undef $/;
+	# my $pdf_string = <$fh>;
+	# my $line;
+	# while (defined( $line = <$fh> )) {
+	# 	print $line, "\n";
+	# }
+	# print $pdf_string;
+	# my $pdf = PDF::API2->open($filename);
+	# my $page = $pdf->openpage(1);
+
 	my $pdf = CAM::PDF->new($filename);
 	my $pageone_tree = $pdf->getPageContentTree(1);
 	print CAM::PDF::PageText->render($pageone_tree);
